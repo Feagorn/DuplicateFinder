badd19ee6f0a26dca9148f973ea19768858c011c
wojciech.moch <wojciech.moch@outlook.com>
2017-02-03 12:47:58 +0100

    Initial commit of the solution.

---
+++ Main/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DF
+{
+    /// <summary>
+    /// This class contains and manages the list of all analyzed and compared files.
+    /// </summary>
+    public class Main
+    {
+
+    }
+}
